0d67061394ec20983174f75c13e480d96e5f997f
Sanechekpirojochek <sasha.ignatov93@gmail.com>
2024-04-06 01:01:21 +0500

    test

---
+++ app/src/main/java/com/example/helloworld/WorldTimeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.helloworld
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class WorldTimeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = WorldTimeFragment()
+    }
+
+    private val viewModel: WorldTimeViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_world_time, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/helloworld/WorldTimeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.helloworld
+
+import androidx.lifecycle.ViewModel
+
+class WorldTimeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
